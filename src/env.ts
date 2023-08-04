120599db131e0e42f0f1ee50b220ce6e82d6be6a
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-08-04 08:38:37 +0000

    M src/env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,12 +1,8 @@
 import { env } from 'extrass'
-
-const result = env({
+export default env({
   DB_URL: r.string(),
   DB_PASS: r.string(),
   BCRYPT_SALT_ROUND: r.number(),
   BCRYPT_SALT_ROUND_2: r.number(),
+  OPTIONAL: r.o.string(),
 })
-
-
-console.log(result)
-export default { ...result }
