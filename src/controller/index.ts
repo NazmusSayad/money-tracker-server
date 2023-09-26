9bd6ce6c05e51bb6944fa5604adbbfc100f59ee8
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-09-26 21:17:32 +0600

    hudai ki jeno fixed

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -5,12 +5,13 @@ import * as token from './auth/tokenController'
 import * as user from './auth/userController'
 
 import * as accounts from './accounts/accountsController'
-import * as transaction from './transaction/transactionController'
-import * as category from './category/categoryController'
+import * as category from './categories/categoriesController'
+import * as transaction from './transactions/transactionsController'
 
 export const authController = catchError(auth)
 export const tokenController = catchError(token)
 export const userController = catchError(user)
+
 export const accountsController = catchError(accounts)
-export const transactionController = catchError(transaction)
 export const categoryController = catchError(category)
+export const transactionController = catchError(transaction)
--- src/router.ts
+++ src/router.ts
@@ -1,15 +1,16 @@
 import { Router } from 'express'
 import authRouter from './routes/authRouter'
 import userRouter from './routes/userRouter'
-import categoryRouter from './routes/categoryRouter'
 import accountsRouter from './routes/accountsRouter'
+import categoryRouter from './routes/categoryRouter'
 import transactionsRouter from './routes/transactionsRouter'
 const router = Router()
 
 router.use('/auth', authRouter)
 router.use('/user', userRouter)
-router.use('/category', categoryRouter)
+
 router.use('/accounts', accountsRouter)
-router.use('/transaction', transactionsRouter)
+router.use('/categories', categoryRouter)
+router.use('/transactions', transactionsRouter)
 
 export default router
