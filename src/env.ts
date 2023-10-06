c0d55910fae7c73a2f0787aee8e5c2f32cac09eb
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-10-07 00:58:05 +0600

    email added

--- src/env.ts
+++ src/env.ts
@@ -4,17 +4,19 @@ import nodeEnv from 'cli-node-env'
 const prodEnv = {
   DB_URL: r.string(),
   DB_PASS: r.string(),
-  JWT_SECRET: r.string(),
   BCRYPT_SALT_ROUND: r.number(),
   BCRYPT_SALT_ROUND_2: r.number(),
+  JWT_SECRET: r.string(),
+  SMTP_SECRET: r.string(),
 }
 
 const devEnv = {
   DB_URL: 'mongodb://127.0.0.1:27017/money-tracker?retryWrites=true&w=majority',
   DB_PASS: '',
-  JWT_SECRET: 'Boom',
   BCRYPT_SALT_ROUND: 2,
   BCRYPT_SALT_ROUND_2: 2,
+  JWT_SECRET: 'Boom',
+  SMTP_SECRET: 'Boom',
 }
 
 export default nodeEnv.isDev ? devEnv : env(prodEnv)
--- src/model/OTP.ts
+++ src/model/OTP.ts
@@ -1,6 +1,8 @@
 import env from '../env'
 import bcrypt from 'bcrypt'
 import mongoose, { HydratedDocumentFromSchema, InferSchemaType } from 'mongoose'
+import User from './User'
+import axios from 'axios'
 
 export type OTPDoc = HydratedDocumentFromSchema<typeof schema>
 export type OTPType = InferSchemaType<typeof schema>
@@ -38,8 +40,25 @@ schema.pre('save', async function (this: PreSaveHelper, next) {
   next()
 })
 
-schema.post('save', function (this: PreSaveHelper) {
-  console.log(this.type, 'code:', this._tempCode)
+schema.post('save', async function (this: PreSaveHelper) {
+  try {
+    const user = await User.findById(this.user)
+    if (!user) return
+
+    await axios.post(
+      'https://smtp.sayad.dev/api/no-reply',
+      {
+        to: user.email,
+        subject: 'Money Tracker OTP',
+        html: `<h1>OTP Code</h1><p>${this._tempCode}</p>`,
+      },
+      {
+        headers: { secret: env.SMTP_SECRET },
+      }
+    )
+  } catch (err) {
+    console.error('Unable to sent email:', err)
+  }
 })
 
 schema.index({ user: 1, type: 1 })
